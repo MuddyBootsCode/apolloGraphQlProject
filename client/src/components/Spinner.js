099bdf5f0ec0f8aa7bb288d3fdd019a527144f54
MuddyBootsCode <firsttogo98@gmail.com>
2018-08-07 17:08:36 -0500

    Refactor: Working on Styling

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -4,7 +4,7 @@ import { RingLoader } from 'react-spinners';
 const Spinner = () => {
     return (
         <div className="spinner">
-            <RingLoader color={'#1eaedb'} size={30} margin={'3px'}/>
+            <RingLoader color={'#1eaedb'} size={60} margin={'3px'}/>
         </div>
     );
 };
